6e0e8d1f6dd0e53b3d6f7fe7de0da8d9ec4a5ea9
eddie ciccone <ecicc2@uis.edu>
2021-10-17 17:41:23 -0500

    removed unneccessary comments

--- src/main/java/edu/uis/recitree/dao/IngredientDAO.java
+++ src/main/java/edu/uis/recitree/dao/IngredientDAO.java
@@ -5,18 +5,9 @@ import edu.uis.recitree.model.Ingredient;
 import java.util.ArrayList;
 
 public interface IngredientDAO {
-    // Create
     Ingredient insertIngredient(Ingredient ingredient);
-
-    // Read all
     ArrayList<Ingredient> selectAllIngredients();
-
-    // Read
     Ingredient selectIngredientByName(Ingredient ingredient);
-
-    // read all unused ingredients
     ArrayList<Ingredient> selectAllUnusedIngredients();
-
-    // Delete
     boolean deleteIngredient(int id);
 }
--- src/main/java/edu/uis/recitree/dao/RecipeDAO.java
+++ src/main/java/edu/uis/recitree/dao/RecipeDAO.java
@@ -6,18 +6,9 @@ import edu.uis.recitree.model.RecipeIngredient;
 import java.util.ArrayList;
 
 public interface RecipeDAO {
-    // create
     Recipe insertRecipe(Recipe recipe);
-
-    // read
     Recipe selectRecipe(int id);
-
-    // read all
     ArrayList<Recipe> selectAllRecipes();
-
-    // update
     Recipe updateRecipe(Recipe recipe);
-
-    // delete
     boolean deleteRecipe(int id);
 }
