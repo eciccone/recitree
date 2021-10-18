f55b352ebf5283b35fbb805057ee024c01707a00
eddie ciccone <ecicc2@uis.edu>
2021-10-18 15:26:11 -0500

    added favorite recipe support to interfaces

--- src/main/java/edu/uis/recitree/service/RecipeService.java
+++ src/main/java/edu/uis/recitree/service/RecipeService.java
@@ -8,9 +8,10 @@ import java.util.ArrayList;
 public interface RecipeService {
     Recipe createRecipe(String name, double servings, ArrayList<RecipeIngredient> ingredients, String instructions);
     Recipe readRecipe(int id);
-    ArrayList<RecipeIngredient> readAllRecipes();
-    ArrayList<RecipeIngredient> searchRecipesByName(String text);
-    ArrayList<RecipeIngredient> searchRecipesByIngredients(String text);
+    ArrayList<Recipe> readAllRecipes();
+    ArrayList<Recipe> readAllFavoriteRecipes();
+    ArrayList<Recipe> searchRecipesByName(String text);
+    ArrayList<Recipe> searchRecipesByIngredients(String text);
     Recipe updateRecipe(int id, String name, double servings, ArrayList<RecipeIngredient> ingredients, String instructions);
     boolean deleteRecipe(int id);
     boolean toggleFavoriteStatus(int id);
