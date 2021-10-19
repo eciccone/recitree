c871727b11522634580cc6a7cc6449863100398d
eddie ciccone <ecicc2@uis.edu>
2021-10-19 17:46:18 -0500

    removed unneeded methods from IngredientService

--- src/main/java/edu/uis/recitree/service/IngredientService.java
+++ src/main/java/edu/uis/recitree/service/IngredientService.java
@@ -5,8 +5,6 @@ import edu.uis.recitree.model.Ingredient;
 import java.util.ArrayList;
 
 public interface IngredientService {
-    Ingredient createIngredient(String name);
-    Ingredient readIngredient(String name);
     ArrayList<Ingredient> readAllUnusedIngredients();
     boolean deleteIngredient(int id);
 }
