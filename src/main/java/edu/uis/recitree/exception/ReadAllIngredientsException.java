93df1a8ce9aff069de3c876f4b0d1958fb858dda
eddie ciccone <ecicc2@uis.edu>
2021-11-03 18:36:48 -0500

    created ReadAllIngredientsException

---
+++ src/main/java/edu/uis/recitree/exception/ReadAllIngredientsException.java
@@ -0,0 +1,8 @@
+package edu.uis.recitree.exception;
+
+public class ReadAllIngredientsException extends Exception {
+
+    public ReadAllIngredientsException(String msg) {
+        super(msg);
+    }
+}
