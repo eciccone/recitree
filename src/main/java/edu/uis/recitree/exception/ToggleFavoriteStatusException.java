3562cfbbaa88a7e58007eb6498a49894d7eb8947
eddie ciccone <ecicc2@uis.edu>
2021-11-03 15:14:21 -0500

    created ToggleFavoriteStatusException

---
+++ src/main/java/edu/uis/recitree/exception/ToggleFavoriteStatusException.java
@@ -0,0 +1,8 @@
+package edu.uis.recitree.exception;
+
+public class ToggleFavoriteStatusException extends Exception {
+
+    public ToggleFavoriteStatusException(String msg) {
+        super(msg);
+    }
+}
