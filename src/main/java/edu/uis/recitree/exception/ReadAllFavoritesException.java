dc7d39ba90af383980270d3003ea10a51ccbd864
eddie ciccone <ecicc2@uis.edu>
2021-11-03 15:22:46 -0500

    created ReadAllFavoritesException

---
+++ src/main/java/edu/uis/recitree/exception/ReadAllFavoritesException.java
@@ -0,0 +1,8 @@
+package edu.uis.recitree.exception;
+
+public class ReadAllFavoritesException extends Exception {
+
+    public ReadAllFavoritesException(String msg) {
+        super(msg);
+    }
+}
