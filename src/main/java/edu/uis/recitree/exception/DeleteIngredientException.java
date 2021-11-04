58cff3186aed7534e77c387ff137a03a1bcbdc64
eddie ciccone <ecicc2@uis.edu>
2021-11-04 16:15:21 -0500

    updated DeleteIngredientException

--- src/main/java/edu/uis/recitree/exception/DeleteIngredientException.java
+++ src/main/java/edu/uis/recitree/exception/DeleteIngredientException.java
@@ -1,2 +1,8 @@
-package edu.uis.recitree.exception;public class DeleteIngredientException {
+package edu.uis.recitree.exception;
+
+public class DeleteIngredientException extends Exception {
+
+    public DeleteIngredientException(String msg) {
+        super(msg);
+    }
 }
