fe05d3a3121e2b54f373511db728ffc7cd136259
eddie ciccone <ecicc2@uis.edu>
2021-11-04 16:22:08 -0500

    created CreateIngredientException

---
+++ src/main/java/edu/uis/recitree/exception/CreateIngredientException.java
@@ -0,0 +1,8 @@
+package edu.uis.recitree.exception;
+
+public class CreateIngredientException extends Exception {
+
+    public CreateIngredientException(String msg) {
+        super(msg);
+    }
+}
